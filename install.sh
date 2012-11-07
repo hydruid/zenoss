eb47432ecbade8a8763cc05b28fdc749961f7f99
Hydruid <info@hydruid-blog.com>
2012-11-07 11:13:30 -0600

    Version-02
    Experimental but functional!

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ sudo mv jdk1.6.0_34 /usr/lib/jvm/
 sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/jdk1.6.0_34/bin/javac 1
 sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk1.6.0_34/bin/java 1
 
-echo "Installing Dependcies"
+echo "Installing Dependencies"
 sudo apt-get install rrdtool mysql-server mysql-client mysql-common libmysqlclient-dev rabbitmq-server nagios-plugins erlang subversion autoconf swig unzip g++ libssl-dev maven libmaven-compiler-plugin-java build-essential libreadline-dev libsnmp-dev zip libssl0.9.8 libxml2-dev libxslt-dev libldap2-dev libsasl2-dev
 
 echo "Zenoss User setup"
@@ -36,18 +36,22 @@ sudo rabbitmqctl add_user zenoss zenoss
 sudo rabbitmqctl add_vhost /zenoss
 sudo rabbitmqctl set_permissions -p /zenoss zenoss '.*' '.*' '.*'
 
-echo "Zenoss Installation Preperation"
+echo "Zenoss Installation Preparation (may take a few minutes)"
 sudo svn --quiet co http://dev.zenoss.org/svn/tags/zenoss-4.2.0/inst /home/zenoss/zenoss-inst
 sudo chown -R zenoss:zenoss /home/zenoss/zenoss-inst
 echo "##	Ready for install!!"
 echo "##	Follow the Instructions below"
 echo "##"
-echo "##	Add/Update the following lines"
-echo "##		sudo vi /home/zenoss/.bashrc"
-echo "##    		export ZENHOME=/usr/local/zenoss      "
-echo "##        	export PYTHONPATH=$ZENHOME/lib/python "
-echo "##            export PATH=$ZENHOME/bin:$PATH        "
-echo "##            export INSTANCE_HOME=$ZENHOME         "
+echo "##	Modify file - sudo vi /home/zenoss/.bashrc"
+echo "##    	export ZENHOME=/usr/local/zenoss"
+echo "##        export PYTHONPATH=$ZENHOME/lib/python"
+echo "##           export PATH=$ZENHOME/bin:$PATH"
+echo "##           export INSTANCE_HOME=$ZENHOME"
+echo "##"
+echo "##	Modify file - sudo vi /etc/mysql/my.cnf"
+echo "##    	max_allowed_packet=16M"
+echo "##        innodb_buffer_pool_size=256M"
+echo "##        innodb_additional_mem_pool_size=20M"
 echo "##"
 echo "##	sudo su zenoss"
 echo "##	cd /home/zenoss/zenoss-inst"
