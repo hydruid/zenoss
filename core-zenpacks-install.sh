06e9d4247268eb22afe3a29e4a4c0a2b8520811f
timothy.lee <marlboromoo@gmail.com>
2012-12-22 10:44:56 +0800

    Convert TAB to 4 space.

--- core-zenpacks-install.sh
+++ core-zenpacks-install.sh
@@ -15,104 +15,104 @@
 echo "Zenpack Core"
 echo "#Zenpacks Source: http://zenpacks.zenoss.com/pypi/none/4.2/ "
 if [ -f ZenPacks.zenoss.ZenJMX-3.8.0-py2.7.egg ];
-	then
-		zenpack --install ZenPacks.zenoss.ApacheMonitor-2.1.3-py2.7.egg
-		zenpack --install ZenPacks.zenoss.DellMonitor-2.2.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.DigMonitor-1.0.3-py2.7.egg
-		zenpack --install ZenPacks.zenoss.DnsMonitor-2.0.3-py2.7.egg	
-		zenpack --install ZenPacks.zenoss.FtpMonitor-1.0.3-py2.7.egg
-		zenpack --install ZenPacks.zenoss.HPMonitor-2.1.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.HttpMonitor-2.0.5-py2.7.egg
-		zenpack --install ZenPacks.zenoss.IRCDMonitor-1.0.3-py2.7.egg
-		zenpack --install ZenPacks.zenoss.JabberMonitor-1.0.3-py2.7.egg
-		zenpack --install ZenPacks.zenoss.LDAPMonitor-1.3.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.MySqlMonitor-2.2.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.NNTPMonitor-1.0.3-py2.7.egg
-		zenpack --install ZenPacks.zenoss.NtpMonitor-2.0.4-py2.7.egg
-		zenpack --install ZenPacks.zenoss.RPCMonitor-1.0.3-py2.7.egg
-		zenpack --install ZenPacks.zenoss.ZenJMX-3.8.0-py2.7.egg 
-		zenpack --install ZenPacks.zenoss.LinuxMonitor-1.2.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.ZenAWS-1.0.7-py2.7.egg
-		zenpack --install ZenPacks.zenoss.ZenossVirtualHostMonitor-2.4.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.EsxTop-1.0.3-py2.7.egg
-		zenpack --install ZenPacks.zenoss.XenMonitor-1.1.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.DeviceSearch-1.2.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.PySamba-1.0.0-py2.7-linux-x86_64.egg
-		zenpack --install ZenPacks.zenoss.WindowsMonitor-1.0.2-py2.7.egg
-		zenpack --install ZenPacks.zenoss.ActiveDirectory-2.1.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.IISMonitor-2.0.2-py2.7.egg
-		zenpack --install ZenPacks.zenoss.MSExchange-2.0.4-py2.7.egg
-		zenpack --install ZenPacks.zenoss.MSMQMonitor-1.2.1-py2.7.egg
-		zenpack --install ZenPacks.zenoss.MSSQLServer-2.0.3-py2.7.egg
-		wget http://hydruid-blog.com/wp-content/uploads/2012/12/pysamba.zip
-		mv pysamba.zip /usr/local/zenoss/ZenPacks/ZenPacks.zenoss.PySamba-1.0.0-py2.7-linux-x86_64.egg/ZenPacks/zenoss/PySamba/lib/
-		cd /usr/local/zenoss/ZenPacks/ZenPacks.zenoss.PySamba-1.0.0-py2.7-linux-x86_64.egg/ZenPacks/zenoss/PySamba/lib/
-		unzip pysamba.zip
-		cp pysamba/easy-install.pth /usr/local/zenoss/ZenPacks/
-		touch /usr/local/zenoss/etc/zenwinperf.conf
-		echo "Restart your server"
-	else
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.PySamba-1.0.0-py2.7-linux-x86_64.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.WindowsMonitor-1.0.2-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.ActiveDirectory-2.1.0-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.ApacheMonitor-2.1.3-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.DellMonitor-2.2.0-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.DeviceSearch-1.2.0-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.DigMonitor-1.0.3-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.DnsMonitor-2.0.3-py2.7.egg	
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.EsxTop-1.0.3-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.FtpMonitor-1.0.3-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.HPMonitor-2.1.0-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.HttpMonitor-2.0.5-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.IISMonitor-2.0.2-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.IRCDMonitor-1.0.3-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.JabberMonitor-1.0.3-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.LDAPMonitor-1.3.0-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.LinuxMonitor-1.2.0-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.ZenossVirtualHostMonitor-2.4.0-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.MSExchange-2.0.4-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.MSMQMonitor-1.2.1-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.MySqlMonitor-2.2.0-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.MSSQLServer-2.0.3-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.NNTPMonitor-1.0.3-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.NtpMonitor-2.0.4-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.RPCMonitor-1.0.3-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.XenMonitor-1.1.0-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.ZenAWS-1.0.7-py2.7.egg
-		wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.ZenJMX-3.8.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.ApacheMonitor-2.1.3-py2.7.egg
-		zenpack --install ZenPacks.zenoss.DellMonitor-2.2.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.DigMonitor-1.0.3-py2.7.egg
-		zenpack --install ZenPacks.zenoss.DnsMonitor-2.0.3-py2.7.egg	
-		zenpack --install ZenPacks.zenoss.FtpMonitor-1.0.3-py2.7.egg
-		zenpack --install ZenPacks.zenoss.HPMonitor-2.1.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.HttpMonitor-2.0.5-py2.7.egg
-		zenpack --install ZenPacks.zenoss.IRCDMonitor-1.0.3-py2.7.egg
-		zenpack --install ZenPacks.zenoss.JabberMonitor-1.0.3-py2.7.egg
-		zenpack --install ZenPacks.zenoss.LDAPMonitor-1.3.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.MySqlMonitor-2.2.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.NNTPMonitor-1.0.3-py2.7.egg
-		zenpack --install ZenPacks.zenoss.NtpMonitor-2.0.4-py2.7.egg
-		zenpack --install ZenPacks.zenoss.RPCMonitor-1.0.3-py2.7.egg
-		zenpack --install ZenPacks.zenoss.ZenJMX-3.8.0-py2.7.egg 
-		zenpack --install ZenPacks.zenoss.LinuxMonitor-1.2.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.ZenAWS-1.0.7-py2.7.egg
-		zenpack --install ZenPacks.zenoss.ZenossVirtualHostMonitor-2.4.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.EsxTop-1.0.3-py2.7.egg
-		zenpack --install ZenPacks.zenoss.XenMonitor-1.1.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.DeviceSearch-1.2.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.PySamba-1.0.0-py2.7-linux-x86_64.egg
-		zenpack --install ZenPacks.zenoss.WindowsMonitor-1.0.2-py2.7.egg
-		zenpack --install ZenPacks.zenoss.ActiveDirectory-2.1.0-py2.7.egg
-		zenpack --install ZenPacks.zenoss.IISMonitor-2.0.2-py2.7.egg
-		zenpack --install ZenPacks.zenoss.MSExchange-2.0.4-py2.7.egg
-		zenpack --install ZenPacks.zenoss.MSMQMonitor-1.2.1-py2.7.egg
-		zenpack --install ZenPacks.zenoss.MSSQLServer-2.0.3-py2.7.egg
-		wget http://hydruid-blog.com/wp-content/uploads/2012/12/pysamba.zip
-		mv pysamba.zip /usr/local/zenoss/ZenPacks/ZenPacks.zenoss.PySamba-1.0.0-py2.7-linux-x86_64.egg/ZenPacks/zenoss/PySamba/lib/
-		cd /usr/local/zenoss/ZenPacks/ZenPacks.zenoss.PySamba-1.0.0-py2.7-linux-x86_64.egg/ZenPacks/zenoss/PySamba/lib/
-		unzip pysamba.zip
-		cp pysamba/easy-install.pth /usr/local/zenoss/ZenPacks/
-		touch /usr/local/zenoss/etc/zenwinperf.conf
-		echo "Restart your server"
+    then
+    zenpack --install ZenPacks.zenoss.ApacheMonitor-2.1.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.DellMonitor-2.2.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.DigMonitor-1.0.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.DnsMonitor-2.0.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.FtpMonitor-1.0.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.HPMonitor-2.1.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.HttpMonitor-2.0.5-py2.7.egg
+    zenpack --install ZenPacks.zenoss.IRCDMonitor-1.0.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.JabberMonitor-1.0.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.LDAPMonitor-1.3.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.MySqlMonitor-2.2.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.NNTPMonitor-1.0.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.NtpMonitor-2.0.4-py2.7.egg
+    zenpack --install ZenPacks.zenoss.RPCMonitor-1.0.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.ZenJMX-3.8.0-py2.7.egg 
+    zenpack --install ZenPacks.zenoss.LinuxMonitor-1.2.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.ZenAWS-1.0.7-py2.7.egg
+    zenpack --install ZenPacks.zenoss.ZenossVirtualHostMonitor-2.4.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.EsxTop-1.0.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.XenMonitor-1.1.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.DeviceSearch-1.2.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.PySamba-1.0.0-py2.7-linux-x86_64.egg
+    zenpack --install ZenPacks.zenoss.WindowsMonitor-1.0.2-py2.7.egg
+    zenpack --install ZenPacks.zenoss.ActiveDirectory-2.1.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.IISMonitor-2.0.2-py2.7.egg
+    zenpack --install ZenPacks.zenoss.MSExchange-2.0.4-py2.7.egg
+    zenpack --install ZenPacks.zenoss.MSMQMonitor-1.2.1-py2.7.egg
+    zenpack --install ZenPacks.zenoss.MSSQLServer-2.0.3-py2.7.egg
+    wget http://hydruid-blog.com/wp-content/uploads/2012/12/pysamba.zip
+    mv pysamba.zip /usr/local/zenoss/ZenPacks/ZenPacks.zenoss.PySamba-1.0.0-py2.7-linux-x86_64.egg/ZenPacks/zenoss/PySamba/lib/
+    cd /usr/local/zenoss/ZenPacks/ZenPacks.zenoss.PySamba-1.0.0-py2.7-linux-x86_64.egg/ZenPacks/zenoss/PySamba/lib/
+    unzip pysamba.zip
+    cp pysamba/easy-install.pth /usr/local/zenoss/ZenPacks/
+    touch /usr/local/zenoss/etc/zenwinperf.conf
+    echo "Restart your server"
+    else
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.PySamba-1.0.0-py2.7-linux-x86_64.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.WindowsMonitor-1.0.2-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.ActiveDirectory-2.1.0-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.ApacheMonitor-2.1.3-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.DellMonitor-2.2.0-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.DeviceSearch-1.2.0-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.DigMonitor-1.0.3-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.DnsMonitor-2.0.3-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.EsxTop-1.0.3-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.FtpMonitor-1.0.3-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.HPMonitor-2.1.0-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.HttpMonitor-2.0.5-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.IISMonitor-2.0.2-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.IRCDMonitor-1.0.3-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.JabberMonitor-1.0.3-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.LDAPMonitor-1.3.0-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.LinuxMonitor-1.2.0-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.ZenossVirtualHostMonitor-2.4.0-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.MSExchange-2.0.4-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.MSMQMonitor-1.2.1-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.MySqlMonitor-2.2.0-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.MSSQLServer-2.0.3-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.NNTPMonitor-1.0.3-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.NtpMonitor-2.0.4-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.RPCMonitor-1.0.3-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.XenMonitor-1.1.0-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.ZenAWS-1.0.7-py2.7.egg
+    wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.ZenJMX-3.8.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.ApacheMonitor-2.1.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.DellMonitor-2.2.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.DigMonitor-1.0.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.DnsMonitor-2.0.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.FtpMonitor-1.0.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.HPMonitor-2.1.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.HttpMonitor-2.0.5-py2.7.egg
+    zenpack --install ZenPacks.zenoss.IRCDMonitor-1.0.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.JabberMonitor-1.0.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.LDAPMonitor-1.3.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.MySqlMonitor-2.2.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.NNTPMonitor-1.0.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.NtpMonitor-2.0.4-py2.7.egg
+    zenpack --install ZenPacks.zenoss.RPCMonitor-1.0.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.ZenJMX-3.8.0-py2.7.egg 
+    zenpack --install ZenPacks.zenoss.LinuxMonitor-1.2.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.ZenAWS-1.0.7-py2.7.egg
+    zenpack --install ZenPacks.zenoss.ZenossVirtualHostMonitor-2.4.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.EsxTop-1.0.3-py2.7.egg
+    zenpack --install ZenPacks.zenoss.XenMonitor-1.1.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.DeviceSearch-1.2.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.PySamba-1.0.0-py2.7-linux-x86_64.egg
+    zenpack --install ZenPacks.zenoss.WindowsMonitor-1.0.2-py2.7.egg
+    zenpack --install ZenPacks.zenoss.ActiveDirectory-2.1.0-py2.7.egg
+    zenpack --install ZenPacks.zenoss.IISMonitor-2.0.2-py2.7.egg
+    zenpack --install ZenPacks.zenoss.MSExchange-2.0.4-py2.7.egg
+    zenpack --install ZenPacks.zenoss.MSMQMonitor-1.2.1-py2.7.egg
+    zenpack --install ZenPacks.zenoss.MSSQLServer-2.0.3-py2.7.egg
+    wget http://hydruid-blog.com/wp-content/uploads/2012/12/pysamba.zip
+    mv pysamba.zip /usr/local/zenoss/ZenPacks/ZenPacks.zenoss.PySamba-1.0.0-py2.7-linux-x86_64.egg/ZenPacks/zenoss/PySamba/lib/
+    cd /usr/local/zenoss/ZenPacks/ZenPacks.zenoss.PySamba-1.0.0-py2.7-linux-x86_64.egg/ZenPacks/zenoss/PySamba/lib/
+    unzip pysamba.zip
+    cp pysamba/easy-install.pth /usr/local/zenoss/ZenPacks/
+    touch /usr/local/zenoss/etc/zenwinperf.conf
+    echo "Restart your server"
 fi
