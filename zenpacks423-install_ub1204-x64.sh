9a204110e45e19178744d44767eb52f0968e1a9a
Hydruid <trey.hunsucker@gmail.com>
2013-01-13 20:17:46 -0600

    Version-01

---
+++ zenpacks423-install_ub1204-x64.sh
@@ -0,0 +1,9 @@
+######################################################
+#!/bin/bash                                          #
+#                                                    #
+# Version: 01                                        #
+# Status: Not Functional...under heavy development   #
+#                                                    #
+# Zenoss Version: Core ZenPacks for 4.2.3            #
+# OS: Ubuntu 12.04 x64                               #
+######################################################
