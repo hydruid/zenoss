a2c6e08b65bb73425786d4a79091a1accae15937
hydruid <trey.hunsucker@gmail.com>
2013-03-04 10:06:57 -0600

    Version: 05a
    Automation and error protection improved

--- zenpacks423-install_ub1204-x64.sh
+++ zenpacks423-install_ub1204-x64.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 #####################################################
-# Version: 04f                                       #
+# Version: 05a                                       #
 # Status: Functional...still in testing              #
 #                                                    #
 # Zenoss Version: Core ZenPacks for 4.2.3            #
 # OS: Ubuntu 12.04 x64                               #
 ######################################################
 
-#Step-01: Download and install the Core Zenpacks
+#Step-01
+if whoami | grep zenoss
+    then
+        echo ""
+    else
+        echo "This script should be ran as the zenoss user..."
+        echo "It's also recommended to run from /home/zenoss"
+        exit 0
+fi
+
+#Step-02: Download and install the Core Zenpacks
 wget http://zenpacks.zenoss.com/pypi/eggs/none/ZenPacks.zenoss.PySamba-1.0.0-py2.7-linux-x86_64.egg
 zenpack --install ZenPacks.zenoss.PySamba-1.0.0-py2.7-linux-x86_64.egg
 declare -a arr=(ZenPacks.zenoss.WindowsMonitor ZenPacks.zenoss.ActiveDirectory ZenPacks.zenoss.ApacheMonitor ZenPacks.zenoss.DellMonitor ZenPacks.zenoss.DeviceSearch ZenPacks.zenoss.DigMonitor ZenPacks.zenoss.DnsMonitor ZenPacks.zenoss.FtpMonitor ZenPacks.zenoss.HPMonitor ZenPacks.zenoss.HttpMonitor ZenPacks.zenoss.IISMonitor ZenPacks.zenoss.IRCDMonitor ZenPacks.zenoss.JabberMonitor ZenPacks.zenoss.LDAPMonitor ZenPacks.zenoss.LinuxMonitor ZenPacks.zenoss.ZenossVirtualHostMonitor ZenPacks.zenoss.MSExchange ZenPacks.zenoss.MSMQMonitor ZenPacks.zenoss.MySqlMonitor ZenPacks.zenoss.MSSQLServer ZenPacks.zenoss.NNTPMonitor ZenPacks.zenoss.NtpMonitor ZenPacks.zenoss.ZenAWS ZenPacks.zenoss.ZenJMX)
@@ -17,7 +27,7 @@ do
 zenpack --fetch $i
 done
 
-#Step02: Apply PySamba, ZenWinPerf, and Easy-Install adjustments
+#Step03: Apply PySamba, ZenWinPerf, and Easy-Install adjustments
 wget https://dl.dropbox.com/s/mbgu0x9fyxhusnv/pysamba.zip
 unzip pysamba.zip
 cp -fr pysamba /usr/local/zenoss/ZenPacks/ZenPacks.zenoss.PySamba-1.0.0-py2.7-linux-x86_64.egg/ZenPacks/zenoss/PySamba/lib
@@ -27,5 +37,5 @@ touch /usr/local/zenoss/etc/zenwinperf.conf
 wget https://dl.dropbox.com/s/p6s4adtpdjibmcl/easy-install.pth
 cp easy-install.pth /usr/local/zenoss/ZenPacks
 
-#Step-03: Restart Zenoss
+#Step-04: Restart Zenoss
 zenoss restart
