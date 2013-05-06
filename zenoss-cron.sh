4c78d568d6483ef51042b0bfa96493bc25a68113
hydruid <trey.hunsucker@gmail.com>
2013-05-06 10:40:23 -0500

    Version: 02
    Minor update to fix issue with ZenWinPerf stopping.

--- zenoss-cron.sh
+++ zenoss-cron.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-
-#################
-# Version: 01   #
-#################
+#
+# Version: 02
+# Notes: This scripts help to ensure that Zenoss starts automatically after a reboot.
+#
 
 if [ -f /usr/local/zenoss/var/zenhub-localhost.pid ];
         then
                 echo "Zenoss is running"
         else
                 echo "Zenoss not running"
-  	/usr/local/zenoss/bin/zenoss restart
+                /usr/local/zenoss/bin/zenoss restart
+fi
+
+if [ -f /usr/local/zenoss/var/zenwinperf-localhost.pid ];
+        then
+                echo "ZenWinPerf is running"
+        else
+                echo "ZenWinPerf not running"
+                /usr/local/zenoss/bin/zenwinperf restart
 fi
 
 exit 0
