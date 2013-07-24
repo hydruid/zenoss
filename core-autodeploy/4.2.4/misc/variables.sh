43c90c86a21dd7a0797a7345cec5d6271c66e8ea
Trey <cnc-admin@ubuntu.(none)>
2013-07-23 21:04:09 -0500

    Version: 02

--- core-autodeploy/4.2.4/misc/variables.sh
+++ core-autodeploy/4.2.4/misc/variables.sh
@@ -1,4 +1,12 @@
+#
+# Version: 02
+# http://hydruid-blog.com/?p=241
+
 export ZENHOME=/usr/local/zenoss
 export PYTHONPATH=/usr/local/zenoss/lib/python
 export PATH=/usr/local/zenoss/bin:$PATH
 export INSTANCE_HOME=$ZENHOME
+
+zodb-admin-password
+zep-admin-password
+
