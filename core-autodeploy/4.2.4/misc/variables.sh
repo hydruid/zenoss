ac783033202ea507b4e59c38f41cdf53f1f7fa8d
Trey H <treyh@ub1204server.(none)>
2013-08-02 13:11:16 -0500

    Version: 02

--- core-autodeploy/4.2.4/misc/variables.sh
+++ core-autodeploy/4.2.4/misc/variables.sh
@@ -1,12 +1,11 @@
-#
-# Version: 02
-# http://hydruid-blog.com/?p=241
+#!/bin/bash
+#######################################################
+# Version: 02a                                        #
+#######################################################
 
+INSTALLDIR="/home/zenoss/zenoss424-srpm_install"
 export ZENHOME=/usr/local/zenoss
 export PYTHONPATH=/usr/local/zenoss/lib/python
 export PATH=/usr/local/zenoss/bin:$PATH
 export INSTANCE_HOME=$ZENHOME
-
-zodb-admin-password
-zep-admin-password
 
