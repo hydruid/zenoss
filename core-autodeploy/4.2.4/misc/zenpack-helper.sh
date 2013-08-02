2d653488cd28b73e0b38891cfe1c99367c56902c
Trey H <treyh@ub1204server.(none)>
2013-08-02 18:15:29 -0500

    Version: 01a

---
+++ core-autodeploy/4.2.4/misc/zenpack-helper.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#######################################################
+# Version: 01a                                        #
+#######################################################
+
+#!/bin/bash
+ZENHOME=$ZENHOME
+export ZENHOME=$ZENHOME
+PYTHONPATH=$ZENHOME/lib/python
+PATH=$ZENHOME/bin:$PATH
+INSTANCE_HOME=$ZENHOME
+$ZENHOME/bin/zenoss restart
+zenpack --install ZenPacks.zenoss.PySamba-1.0.2-py2.7-linux-x86_64.egg
+zenpack --install ZenPacks.zenoss.WindowsMonitor-1.0.8-py2.7.egg
+zenpack --install ZenPacks.zenoss.ActiveDirectory-2.1.0-py2.7.egg
+zenpack --install ZenPacks.zenoss.ApacheMonitor-2.1.3-py2.7.egg
+zenpack --install ZenPacks.zenoss.DellMonitor-2.2.0-py2.7.egg
+zenpack --install ZenPacks.zenoss.DeviceSearch-1.2.0-py2.7.egg
+zenpack --install ZenPacks.zenoss.DigMonitor-1.1.0-py2.7.egg
+zenpack --install ZenPacks.zenoss.DnsMonitor-2.1.0-py2.7.egg
+zenpack --install ZenPacks.zenoss.EsxTop-1.1.0-py2.7.egg
+zenpack --install ZenPacks.zenoss.FtpMonitor-1.1.0-py2.7.egg
+zenpack --install ZenPacks.zenoss.HPMonitor-2.1.0-py2.7.egg
+zenpack --install ZenPacks.zenoss.HttpMonitor-2.1.0-py2.7.egg
+zenpack --install ZenPacks.zenoss.IISMonitor-2.0.2-py2.7.egg
+zenpack --install ZenPacks.zenoss.IRCDMonitor-1.1.0-py2.7.egg
+zenpack --install ZenPacks.zenoss.JabberMonitor-1.1.0-py2.7.egg
+zenpack --install ZenPacks.zenoss.LDAPMonitor-1.4.0-py2.7.egg
+zenpack --install ZenPacks.zenoss.LinuxMonitor-1.2.1-py2.7.egg
+zenpack --install ZenPacks.zenoss.MSExchange-2.0.4-py2.7.egg
+zenpack --install ZenPacks.zenoss.MSMQMonitor-1.2.1-py2.7.egg
+zenpack --install ZenPacks.zenoss.MSSQLServer-2.0.3-py2.7.egg
+zenpack --install ZenPacks.zenoss.MySqlMonitor-2.2.0-py2.7.egg
+zenpack --install ZenPacks.zenoss.NNTPMonitor-1.1.0-py2.7.egg
+zenpack --install ZenPacks.zenoss.NtpMonitor-2.2.0-py2.7.egg
+zenpack --install ZenPacks.zenoss.PythonCollector-1.0.1-py2.7.egg
+zenpack --install ZenPacks.zenoss.WBEM-1.0.0-py2.7.egg
+zenpack --install ZenPacks.zenoss.WindowsMonitor-1.0.8-py2.7.egg
+zenpack --install ZenPacks.zenoss.XenMonitor-1.1.0-py2.7.egg
+zenpack --install ZenPacks.zenoss.ZenJMX-3.9.5-py2.7.egg
+zenpack --install ZenPacks.zenoss.ZenossVirtualHostMonitor-2.4.0-py2.7.egg
+easy_install readline
+$ZENHOME/bin/zenoss restart
