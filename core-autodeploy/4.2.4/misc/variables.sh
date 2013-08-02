2e212e8d4b52a03e7df5a4f6c5abaa0fb4c9a906
Trey H <treyh@ub1204server.(none)>
2013-08-02 13:16:02 -0500

    Version: 02a

--- core-autodeploy/4.2.4/misc/variables.sh
+++ core-autodeploy/4.2.4/misc/variables.sh
@@ -4,6 +4,9 @@
 #######################################################
 
 INSTALLDIR="/home/zenoss/zenoss424-srpm_install"
+QUIET="> /dev/null 2>/dev/null"
+
+
 export ZENHOME=/usr/local/zenoss
 export PYTHONPATH=/usr/local/zenoss/lib/python
 export PATH=/usr/local/zenoss/bin:$PATH
