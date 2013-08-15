8f8e4c4db682b6dbfd25d755b4ec06dbe7595d4d
Trey H <treyh@ub1204server.(none)>
2013-08-15 08:30:24 -0500

    Version: 02b Alpha - 12

--- core-autodeploy/4.2.4/zenoss424_ub1204_alpha.sh
+++ core-autodeploy/4.2.4/zenoss424_ub1204_alpha.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #######################################################
-# Version: 02b Alpha - 11                             #
+# Version: 02b Alpha - 12                             #
 #  Status: Functional...Issues with 1st device model  #
 #   Notes: Updating code to resolve MySQL pass bug    #
 #  Zenoss: Core 4.2.4 & ZenPacks                      #
@@ -13,9 +13,7 @@
 # Beginning Script Message
 echo && echo "Welcome to the Zenoss 4.2.4 core-autodeploy script for Ubuntu!"
 echo "Blog Post: http://hydruid-blog.com/?p=124" && echo && echo
-echo "PLEASE UPDATE your Ubuntu install before running this script!"
-echo "...A simple Ubuntu update script: http://hydruid-blog.com/?p=200" && echo
-sleep 10
+sleep 5
 
 # Script Compatibility with OS
 if grep -Fxq "Ubuntu 12.04.2 LTS" /etc/issue.net
@@ -33,8 +31,11 @@ fi
 
 # Install Package Dependencies
 apt-get update
+apt-get dist-upgrade -y
+apt-get autoremove
 apt-get install python-software-properties -y && sleep 1
 echo | add-apt-repository ppa:webupd8team/java && sleep 1
+apt-get update
 array=( rrdtool libmysqlclient-dev rabbitmq-server nagios-plugins erlang subversion autoconf swig unzip zip g++ libssl-dev maven libmaven-compiler-plugin-java build-essential libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev oracle-java7-installer python-twisted python-gnutls python-twisted-web python-samba libsnmp-base snmp-mibs-downloader bc rpm2cpio memcached libncurses5 libncurses5-dev libreadline6-dev libreadline6 librrd-dev python-setuptools python-dev )
 for i in "${array[@]}"
 do
