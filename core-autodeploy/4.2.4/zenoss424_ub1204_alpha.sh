a9efbbb9945d5d97c497de35abe3811c64f34768
Trey <cnc-admin@ubuntu.(none)>
2013-08-20 20:51:28 -0500

    Version: 02b Alpha - 15

--- core-autodeploy/4.2.4/zenoss424_ub1204_alpha.sh
+++ core-autodeploy/4.2.4/zenoss424_ub1204_alpha.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #######################################################
-# Version: 02b Alpha - 13                             #
+# Version: 02b Alpha - 15                             #
 #  Status: Not Functional...issues with rabbit        #
 #   Notes: Updating code to resolve MySQL pass bug    #
 #  Zenoss: Core 4.2.4 & ZenPacks                      #
@@ -12,11 +12,10 @@
 
 # Beginning Script Message
 echo && echo "Welcome to the Zenoss 4.2.4 core-autodeploy script for Ubuntu!"
-echo "Blog Post: http://hydruid-blog.com/?p=124" && echo && echo
-sleep 5
+echo "Blog Post: http://hydruid-blog.com/?p=124" && echo && echo && sleep 5
 
 # Script Compatibility with OS
-if grep -Fxq "Ubuntu 12.04.2 LTS" /etc/issue.net
+if grep -Fxq "Ubuntu 12.04.3 LTS" /etc/issue.net
         then    echo "...Correct OS detected."
         else    echo "...Incorrect OS detected...stopping script" && exit 0
 fi
@@ -36,17 +35,11 @@ apt-get autoremove
 apt-get install python-software-properties -y && sleep 1
 echo | add-apt-repository ppa:webupd8team/java && sleep 1
 apt-get update
-array=( rrdtool libmysqlclient-dev rabbitmq-server nagios-plugins erlang subversion autoconf swig unzip zip g++ libssl-dev maven libmaven-compiler-plugin-java build-essential libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev oracle-java7-installer python-twisted python-gnutls python-twisted-web python-samba libsnmp-base snmp-mibs-downloader bc rpm2cpio memcached libncurses5 libncurses5-dev libreadline6-dev libreadline6 librrd-dev python-setuptools python-dev )
-for i in "${array[@]}"
-do
-        apt-get install $i -y && sleep 1
-done
+apt-get install rrdtool libmysqlclient-dev nagios-plugins erlang subversion autoconf swig unzip zip g++ libssl-dev maven libmaven-compiler-plugin-java build-essential libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev oracle-java7-installer python-twisted python-gnutls python-twisted-web python-samba libsnmp-base snmp-mibs-downloader bc rpm2cpio memcached libncurses5 libncurses5-dev libreadline6-dev libreadline6 librrd-dev python-setuptools python-dev erlang-nox -y
+wget -N http://www.rabbitmq.com/releases/rabbitmq-server/v3.1.3/rabbitmq-server_3.1.3-1_all.deb
+dpkg -i rabbitmq-server_3.1.3-1_all.deb
 export DEBIAN_FRONTEND=noninteractive
-array=( mysql-server mysql-client mysql-common )
-for i in "${array[@]}"
-do
-        apt-get install $i -y && sleep 1
-done
+apt-get install mysql-server mysql-client mysql-common -y 
 mysql -u root -e "show databases;" > /tmp/mysql.txt 2>> /tmp/mysql.txt
 if grep -Fxq "Database" /tmp/mysql.txt
         then    echo "...MySQL connection test successful."
@@ -74,7 +67,6 @@ echo '#max_allowed_packet=16M' >> /etc/mysql/my.cnf
 echo 'innodb_buffer_pool_size=256M' >> /etc/mysql/my.cnf
 echo 'innodb_additional_mem_pool_size=20M' >> /etc/mysql/my.cnf
 sed -i 's/mibs/#mibs/g' /etc/snmp/snmp.conf
-echo "...It's safe to ignore the above rabbit warnings."
 
 # Download Zenoss SRPM and extract it
 if [ -f $INSTALLDIR/zenoss_core-4.2.4/GNUmakefile.in ];
@@ -96,8 +88,6 @@ make
 make install
 cd $INSTALLDIR/zenoss_core-4.2.4/
 wget -N http://dev.zenoss.org/svn/tags/zenoss-4.2.4/inst/rrdclean.sh
-#wget -N http://www.rabbitmq.com/releases/rabbitmq-server/v3.1.3/rabbitmq-server_3.1.3-1_all.deb
-#dpkg -i rabbitmq-server_3.1.3-1_all.deb
 ./configure 2>&1 | tee log-configure.log
 make 2>&1 | tee log-make.log
 make clean 2>&1 | tee log-make_clean.log
@@ -106,7 +96,6 @@ su - root -c "sed -i 's:# configure to generate the uplevel mkzenossinstance.sh 
 ./mkzenossinstance.sh 2>&1 | tee log-mkzenossinstance_a.log
 ./mkzenossinstance.sh 2>&1 | tee log-mkzenossinstance_b.log
 chown -R zenoss:zenoss $ZENHOME
-echo "...It's safe to ignore the above pyraw,zensocket,nmap warnings."
 
 # Install Zenoss Core Zenpacks
 rm -fr /home/zenoss/rpm > /dev/null 2>/dev/null
