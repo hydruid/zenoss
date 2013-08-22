d47daf79ca3e278eb4c22531c196d18f9c98bda3
Trey H <treyh@ub1204server.(none)>
2013-08-21 19:49:54 -0500

    Version: 02b

--- core-autodeploy/4.2.4/misc/variables.sh
+++ core-autodeploy/4.2.4/misc/variables.sh
@@ -11,9 +11,19 @@ PATH=/usr/local/zenoss/bin:$PATH
 INSTANCE_HOME=$ZENHOME
 
 # Functions
-detectos () {
+detect-os () {
 if grep -Fxq "Ubuntu 12.04.3 LTS" /etc/issue.net
         then    echo "...Correct OS detected."
         else    echo "...Incorrect OS detected...stopping script" && exit 0
-fi
-}
+fi	}
+detect-arch () {
+if uname -m | grep -Fxq "x86_64"
+        then    echo "...Correct Arch detected."
+        else    echo "...Incorrect Arch detected...stopped script" && exit 0
+fi	}
+detect-user () {
+if [ `whoami` != 'zenoss' ];
+        then    echo "...All system checks passed."
+        else    echo "...This script should not be ran by the zenoss user" && exit 0
+fi	}
+
