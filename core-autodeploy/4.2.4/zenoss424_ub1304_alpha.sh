6f0a7d35aa61b8dfe45d788460d963ba116a70b6
Hydruid <trey.hunsucker@gmail.com>
2013-09-08 19:57:57 -0500

    Version: 01b Alpha - 03

--- core-autodeploy/4.2.4/zenoss424_ub1304_alpha.sh
+++ core-autodeploy/4.2.4/zenoss424_ub1304_alpha.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #######################################################
-# Version: 01b Alpha - 02                             #
+# Version: 01b Alpha - 03                             #
 #  Status: Not Functional                             #
 #   Notes: zeneventd won't start...python segfault    #
-#  Zenoss: Core 4.2.4 & ZenPacks                      #
-#      OS: Ubuntu 12.04.2 x86_64                      #
+#  Zenoss: Core 4.2.4 & ZenPacks (v1897)              #
+#      OS: Ubuntu 13.04 x86_64                        #
 #######################################################
 
 # Beginning Script Message
 echo && echo "Welcome to the Zenoss 4.2.4 core-autodeploy script for Ubuntu!"
-echo "Blog Post: http://hydruid-blog.com/?p=343" && echo && sleep 5
+echo "Blog Post: http://hydruid-blog.com/?p=124" && echo && sleep 5
 
 # Update Ubuntu
 apt-get update && apt-get dist-upgrade -y && apt-get autoremove -y
@@ -31,9 +31,6 @@ mkdir $ZENHOME && chown -cR zenoss:zenoss $ZENHOME
 detect-os2 && detect-arch && detect-user
 
 # Install Package Dependencies
-wget http://security.ubuntu.com/ubuntu/pool/main/e/eglibc/libc-bin_2.15-0ubuntu10.2_amd64.deb
-dpkg -i libc-bin_2.15-0ubuntu10.2_amd64.deb
-sudo apt-mark hold libc-bin
 apt-get install python-software-properties -y && sleep 1
 echo | add-apt-repository ppa:webupd8team/java && sleep 1 && apt-get update
 apt-get install rrdtool libmysqlclient-dev nagios-plugins erlang subversion autoconf swig unzip zip g++ libssl-dev maven libmaven-compiler-plugin-java build-essential libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev oracle-java7-installer python-twisted python-gnutls python-twisted-web python-samba libsnmp-base snmp-mibs-downloader bc rpm2cpio memcached libncurses5 libncurses5-dev libreadline6-dev libreadline6 librrd-dev python-setuptools python-dev erlang-nox -y
@@ -50,9 +47,9 @@ rabbitmqctl add_vhost /zenoss
 rabbitmqctl set_permissions -p /zenoss zenoss '.*' '.*' '.*'
 
 # Download Zenoss SRPM and extract it
-wget -N http://iweb.dl.sourceforge.net/project/zenoss/zenoss-4.2/zenoss-4.2.4/zenoss_core-4.2.4.el6.src.rpm -P $INSTALLDIR && cd $INSTALLDIR
-rpm2cpio zenoss_core-4.2.4.el6.src.rpm | cpio -i --make-directories
-bunzip2 zenoss_core-4.2.4-1859.el6.x86_64.tar.bz2 && tar -xvf zenoss_core-4.2.4-1859.el6.x86_64.tar
+wget -N http://softlayer-dal.dl.sourceforge.net/project/zenoss/zenoss-4.2/zenoss-4.2.4/4.2.4-1897/zenoss_core-4.2.4-1897.el6.src.rpm -P $INSTALLDIR && cd $INSTALLDIR
+rpm2cpio zenoss_core-4.2.4-1897.el6.src.rpm | cpio -i --make-directories
+bunzip2 zenoss_core-4.2.4-1897.el6.x86_64.tar.bz2 && tar -xvf zenoss_core-4.2.4-1897.el6.x86_64.tar
 chown -R zenoss:zenoss $INSTALLDIR
 
 # Install Zenoss Core
@@ -71,8 +68,8 @@ chown -R zenoss:zenoss $ZENHOME
 
 # Install Zenoss Core Zenpacks
 mkdir $INSTALLDIR/ZenPacks && cd $INSTALLDIR/ZenPacks
-wget -N http://superb-dca2.dl.sourceforge.net/project/zenoss/zenoss-4.2/zenoss-4.2.4/zenoss_core-4.2.4.el6.x86_64.rpm -P $INSTALLDIR/ZenPacks
-rpm2cpio zenoss_core-4.2.4.el6.x86_64.rpm | sudo cpio -ivd ./opt/zenoss/packs/*.*
+wget -N http://hivelocity.dl.sourceforge.net/project/zenoss/zenoss-4.2/zenoss-4.2.4/4.2.4-1897/zenoss_core-4.2.4-1897.el6.x86_64.rpm -P $INSTALLDIR/ZenPacks
+rpm2cpio zenoss_core-4.2.4-1897.el6.x86_64.rpm | sudo cpio -ivd ./opt/zenoss/packs/*.*
 wget -N https://raw.github.com/hydruid/zenoss/master/core-autodeploy/4.2.4/misc/zenpack-helper.sh -P $INSTALLDIR/ZenPacks/opt/zenoss/packs
 chown -R zenoss:zenoss /home/zenoss
 su - zenoss -c "cd $INSTALLDIR/ZenPacks/opt/zenoss/packs && /bin/sh zenpack-helper.sh"
