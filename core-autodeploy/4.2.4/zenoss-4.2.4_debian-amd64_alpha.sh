264f28cfacc02de9dae643d69200db1605385879
Trey Hunsucker <trey.hunsucker@gmail.com>
2013-10-02 14:52:00 -0500

    Version: 01a Alpha - 02

--- core-autodeploy/4.2.4/zenoss-4.2.4_debian-amd64_alpha.sh
+++ core-autodeploy/4.2.4/zenoss-4.2.4_debian-amd64_alpha.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #######################################################
-# Version: 01a Alpha - 01                             #
+# Version: 01a Alpha - 02                             #
 #  Status: Not Functional                             #
-#   Notes: Started work on the basics                 #
+#   Notes: Various daemons are broken                 #
 #  Zenoss: Core 4.2.4 & ZenPacks (v1897)              #
 #      OS: Debian 7 x86_64                            #
 #######################################################
@@ -43,7 +43,26 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get install mysql-server mysql-client mysql-common -y
 mysql-conn_test
 
-echo "...stopping here"
-echo "...still generating a DEB for Debian, can not use the Ubuntu DEB"
+# Download Zenoss SRPM and extract it
+wget -N http://softlayer-dal.dl.sourceforge.net/project/zenoss/zenoss-4.2/zenoss-4.2.4/4.2.4-1897/zenoss_core-4.2.4-1897.el6.src.rpm -P $INSTALLDIR && cd $INSTALLDIR
+rpm2cpio zenoss_core-4.2.4-1897.el6.src.rpm | cpio -i --make-directories
+bunzip2 zenoss_core-4.2.4-1897.el6.x86_64.tar.bz2 && tar -xvf zenoss_core-4.2.4-1897.el6.x86_64.tar
+chown -R zenoss:zenoss $INSTALLDIR
 
+# Install Zenoss Core
+tar zxvf $INSTALLDIR/zenoss_core-4.2.4/externallibs/rrdtool-1.4.7.tar.gz -C $INSTALLDIR/ && cd $INSTALLDIR/rrdtool-1.4.7
+./configure --prefix=$ZENHOME
+make && make install
+wget -N http://dev.zenoss.org/svn/tags/zenoss-4.2.4/inst/rrdclean.sh -P $INSTALLDIR/zenoss_core-4.2.4/ && cd $INSTALLDIR/zenoss_core-4.2.4/
+./configure 2>&1 | tee log-configure.log
+make 2>&1 | tee log-make.log
+make clean 2>&1 | tee log-make_clean.log
+cp mkzenossinstance.sh mkzenossinstance.sh.orig
+su - root -c "sed -i 's:# configure to generate the uplevel mkzenossinstance.sh script.:# configure to generate the uplevel mkzenossinstance.sh script.\n#\n#Custom Ubuntu Variables\n. /home/zenoss/zenoss424-srpm_install/variables.sh:g' $INSTALLDIR/zenoss_core-4.2.4/mkzenossinstance.sh"
+./mkzenossinstance.sh 2>&1 | tee log-mkzenossinstance_a.log
+./mkzenossinstance.sh 2>&1 | tee log-mkzenossinstance_b.log
+chown -R zenoss:zenoss $ZENHOME
+
+echo "...stopping here"
+echo "...this section still under development"
 exit 0
