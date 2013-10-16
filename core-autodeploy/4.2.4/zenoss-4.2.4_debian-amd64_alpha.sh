c9aa0bb28cf5715a6b6d3c4ebcfafd793ff91850
hydruid <trey.hunsucker@gmail.com>
2013-10-16 16:26:04 -0500

    Version: 01a Alpha - 03

--- core-autodeploy/4.2.4/zenoss-4.2.4_debian-amd64_alpha.sh
+++ core-autodeploy/4.2.4/zenoss-4.2.4_debian-amd64_alpha.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #######################################################
-# Version: 01a Alpha - 02                             #
+# Version: 01a Alpha - 03                             #
 #  Status: Not Functional                             #
 #   Notes: Various daemons are broken                 #
 #  Zenoss: Core 4.2.4 & ZenPacks (v1897)              #
@@ -11,7 +11,7 @@
 echo && echo "Welcome to the Zenoss 4.2.4 core-autodeploy script for Debian!"
 echo "Blog Post: http://hydruid-blog.com/?p=343" && echo && sleep 5
 
-# Update Ubuntu
+# Update Debian
 apt-get update && apt-get dist-upgrade -y && apt-get autoremove -y
 
 # Setup zenoss user and build environment
@@ -28,6 +28,10 @@ wget -N https://raw.github.com/hydruid/zenoss/master/core-autodeploy/4.2.4/misc/
 mkdir $ZENHOME && chown -cR zenoss:zenoss $ZENHOME
 
 # OS compatibility tests
+if grep -Fxq "testing" /etc/apt/sources.list
+        then echo "...Detected testing repo."
+        else echo "...Did not detect testing repo, see https://wiki.debian.org/DebianTesting" && exit 0
+fi
 detect-os2 && detect-arch && detect-user
 
 # Install Package Dependencies
@@ -37,32 +41,47 @@ echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | t
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
 apt-get update
 apt-get install rrdtool libmysqlclient-dev nagios-plugins erlang subversion autoconf swig unzip zip g++ libssl-dev maven libmaven-compiler-plugin-java build-essential libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev oracle-java7-installer python-twisted python-gnutls python-twisted-web python-samba libsnmp-base bc rpm2cpio memcached libncurses5 libncurses5-dev libreadline6-dev libreadline6 librrd-dev python-setuptools python-dev erlang-nox smistrip -y
+apt-get -t testing install libc6
 wget -N http://ftp.us.debian.org/debian/pool/non-free/s/snmp-mibs-downloader/snmp-mibs-downloader_1.1_all.deb
 dpkg -i snmp-mibs-downloader_1.1_all.deb
 export DEBIAN_FRONTEND=noninteractive
 apt-get install mysql-server mysql-client mysql-common -y
 mysql-conn_test
 
-# Download Zenoss SRPM and extract it
-wget -N http://softlayer-dal.dl.sourceforge.net/project/zenoss/zenoss-4.2/zenoss-4.2.4/4.2.4-1897/zenoss_core-4.2.4-1897.el6.src.rpm -P $INSTALLDIR && cd $INSTALLDIR
-rpm2cpio zenoss_core-4.2.4-1897.el6.src.rpm | cpio -i --make-directories
-bunzip2 zenoss_core-4.2.4-1897.el6.x86_64.tar.bz2 && tar -xvf zenoss_core-4.2.4-1897.el6.x86_64.tar
-chown -R zenoss:zenoss $INSTALLDIR
-
-# Install Zenoss Core
-tar zxvf $INSTALLDIR/zenoss_core-4.2.4/externallibs/rrdtool-1.4.7.tar.gz -C $INSTALLDIR/ && cd $INSTALLDIR/rrdtool-1.4.7
-./configure --prefix=$ZENHOME
-make && make install
-wget -N http://dev.zenoss.org/svn/tags/zenoss-4.2.4/inst/rrdclean.sh -P $INSTALLDIR/zenoss_core-4.2.4/ && cd $INSTALLDIR/zenoss_core-4.2.4/
-./configure 2>&1 | tee log-configure.log
-make 2>&1 | tee log-make.log
-make clean 2>&1 | tee log-make_clean.log
-cp mkzenossinstance.sh mkzenossinstance.sh.orig
-su - root -c "sed -i 's:# configure to generate the uplevel mkzenossinstance.sh script.:# configure to generate the uplevel mkzenossinstance.sh script.\n#\n#Custom Ubuntu Variables\n. /home/zenoss/zenoss424-srpm_install/variables.sh:g' $INSTALLDIR/zenoss_core-4.2.4/mkzenossinstance.sh"
-./mkzenossinstance.sh 2>&1 | tee log-mkzenossinstance_a.log
-./mkzenossinstance.sh 2>&1 | tee log-mkzenossinstance_b.log
+# Download Zenoss DEB and install it
+wget -N hydruid-blog.com/zenoss-core-4.2.4-1897.ubuntu.x86-64_01a_amd64.deb
+dpkg -i zenoss-core-4.2.4-1897.ubuntu.x86-64_01a_amd64.deb
 chown -R zenoss:zenoss $ZENHOME
+
+# Import the MySQL Database and create users
+mysql -u root -e "create database zenoss_zep"
+mysql -u root -e "create database zodb"
+mysql -u root -e "create database zodb_session"
+mysql -u root zenoss_zep < /home/zenoss/zenoss_zep.sql
+mysql -u root zodb < /home/zenoss/zodb.sql
+mysql -u root zodb_session < /home/zenoss/zodb_session.sql
+mysql -u root -e "CREATE USER 'zenoss'@'localhost' IDENTIFIED BY  'zenoss';"
+mysql -u root -e "GRANT REPLICATION SLAVE ON *.* TO 'zenoss'@'localhost' IDENTIFIED BY PASSWORD '*3715D7F2B0C1D26D72357829DF94B81731174B8C';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON zodb.* TO 'zenoss'@'localhost';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON zenoss_zep.* TO 'zenoss'@'localhost';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON zodb_session.* TO 'zenoss'@'localhost';"
+mysql -u root -e "GRANT SELECT ON mysql.proc TO 'zenoss'@'localhost';"
+mysql -u root -e "CREATE USER 'zenoss'@'%' IDENTIFIED BY  'zenoss';"
+mysql -u root -e "GRANT REPLICATION SLAVE ON *.* TO 'zenoss'@'%' IDENTIFIED BY PASSWORD '*3715D7F2B0C1D26D72357829DF94B81731174B8C';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON zodb.* TO 'zenoss'@'%';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON zenoss_zep.* TO 'zenoss'@'%';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON zodb_session.* TO 'zenoss'@'%';"
+mysql -u root -e "GRANT SELECT ON mysql.proc TO 'zenoss'@'%';"
+
+# Rabbit install and config
+wget -N http://www.rabbitmq.com/releases/rabbitmq-server/v3.1.5/rabbitmq-server_3.1.5-1_all.deb -P /home/zenoss/zenoss424-srpm_install/
+dpkg -i /home/zenoss/zenoss424-srpm_install/rabbitmq-server_3.1.5-1_all.deb
+chown -R zenoss:zenoss $ZENHOME
+rabbitmqctl add_user zenoss zenoss
+rabbitmqctl add_vhost /zenoss
+rabbitmqctl set_permissions -p /zenoss zenoss '.*' '.*' '.*'
 
 echo "...stopping here"
 echo "...this section still under development"
+echo "...su to the zenoss user and issue, zenoss start"
 exit 0
