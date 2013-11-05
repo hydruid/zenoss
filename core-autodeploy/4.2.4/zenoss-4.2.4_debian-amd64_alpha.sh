610ab7cc6f41ed469a01f21e49c541d41937f59d
hydruid <trey.hunsucker@gmail.com>
2013-11-05 13:23:42 -0600

    Version: 02a Alpha - 04

--- core-autodeploy/4.2.4/zenoss-4.2.4_debian-amd64_alpha.sh
+++ core-autodeploy/4.2.4/zenoss-4.2.4_debian-amd64_alpha.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #######################################################
-# Version: 02a Alpha - 03                             #
+# Version: 02a Alpha - 04                             #
 #  Status: Not Functional                             #
 #   Notes: Working on SSL Lib bug for Debian 6        #
 #  Zenoss: Core 4.2.4 & ZenPacks (v1897)              #
@@ -100,6 +100,20 @@ touch $ZENHOME/var/Data.fs
 cp $ZENHOME/bin/zenoss /etc/init.d/zenoss
 su - root -c "sed -i 's:# License.zenoss under the directory where your Zenoss product is installed.:# License.zenoss under the directory where your Zenoss product is installed.\n#\n#Custom Ubuntu Variables\nexport ZENHOME=$ZENHOME\nexport RRDCACHED=$ZENHOME/bin/rrdcached:g' /etc/init.d/zenoss"
 update-rc.d zenoss defaults && sleep 2
+touch /etc/insserv/overrides/zenoss
+cat > /etc/insserv/overrides/zenoss << EOL
+### BEGIN INIT INFO
+# Provides: zenoss-stack
+# Required-Start: $local_fs $remote_fs
+# Required-Stop: $local_fs $remote_fs
+# Should-Start: $all
+# Should-Stop: $all
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: Start/stop Zenoss-stack
+# Description: Start/stop Zenoss-stack
+### END INIT INFO
+EOL
 chown -c root:zenoss /usr/local/zenoss/bin/pyraw
 chown -c root:zenoss /usr/local/zenoss/bin/zensocket
 chown -c root:zenoss /usr/local/zenoss/bin/nmap
