0741b3bc8d776b2d38b7aeb8b33d629ce4020a5d
Hydruid <trey.hunsucker@gmail.com>
2013-11-14 15:16:00 -0600

    Version: 02a Alpha - 02

--- core-autodeploy/4.2.4/zenoss-4.2.4_ubuntu-amd64_alpha.sh
+++ core-autodeploy/4.2.4/zenoss-4.2.4_ubuntu-amd64_alpha.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 #######################################################
-# Version: 02a Alpha - 01                             #
-#  Status: Under Development...                       #
-#   Notes: Plan to clean the code up...               #
+# Version: 02a Alpha - 02                             #
 #  Status: Functional                                 #
+#   Notes: Mostly just code cleanup...                #
 #      OS: Ubuntu 12/13 x86_64                        #
 #######################################################
 
 # Beginning Script Message
 echo && echo "Welcome to the Zenoss 4.2.4 core-autodeploy script for Ubuntu!"
-echo "Blog Post: http://hydruid-blog.com/?p=343" && echo && sleep 5
+echo "Blog Post: http://hydruid-blog.com/?p=343" && echo 
+echo "Notes: Ubuntu Rocks and I appreciate any feedback!!!" && echo && sleep 5
+
+# Installer variables
+## Home path for the zenoss user
+zenosshome="/home/zenoss"
 
 # Update Ubuntu
 apt-get update && apt-get dist-upgrade -y && apt-get autoremove -y
 
 # Setup zenoss user and build environment
 useradd -m -U -s /bin/bash zenoss
-chmod 777 /home/zenoss/.bashrc
-echo 'export ZENHOME=/usr/local/zenoss' >> /home/zenoss/.bashrc
-echo 'export PYTHONPATH=/usr/local/zenoss/lib/python' >> /home/zenoss/.bashrc
-echo 'export PATH=/usr/local/zenoss/bin:$PATH' >> /home/zenoss/.bashrc
-echo 'export INSTANCE_HOME=$ZENHOME' >> /home/zenoss/.bashrc
-chmod 644 /home/zenoss/.bashrc
-mkdir /home/zenoss/zenoss424-srpm_install
-wget -N https://raw.github.com/hydruid/zenoss/master/core-autodeploy/4.2.4/misc/variables.sh -P /home/zenoss/zenoss424-srpm_install/
-. /home/zenoss/zenoss424-srpm_install/variables.sh
+chmod 777 $zenosshome/.bashrc
+echo 'export ZENHOME=/usr/local/zenoss' >> $zenosshome/.bashrc
+echo 'export PYTHONPATH=/usr/local/zenoss/lib/python' >> $zenosshome/.bashrc
+echo 'export PATH=/usr/local/zenoss/bin:$PATH' >> $zenosshome/.bashrc
+echo 'export INSTANCE_HOME=$ZENHOME' >> $zenosshome/.bashrc
+chmod 644 $zenosshome/.bashrc
+mkdir $zenosshome/zenoss424-srpm_install
+wget -N https://raw.github.com/hydruid/zenoss/master/core-autodeploy/4.2.4/misc/variables.sh -P $zenosshome/zenoss424-srpm_install/
+. $zenosshome/zenoss424-srpm_install/variables.sh
 mkdir $ZENHOME && chown -cR zenoss:zenoss $ZENHOME
 
 # OS compatibility tests
@@ -34,9 +38,11 @@ detect-os2 && detect-arch && detect-user
 apt-get install python-software-properties -y && sleep 1
 echo | add-apt-repository ppa:webupd8team/java && sleep 1 && apt-get update
 apt-get install rrdtool libmysqlclient-dev nagios-plugins erlang subversion autoconf swig unzip zip g++ libssl-dev maven libmaven-compiler-plugin-java build-essential libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev oracle-java7-installer python-twisted python-gnutls python-twisted-web python-samba libsnmp-base snmp-mibs-downloader bc rpm2cpio memcached libncurses5 libncurses5-dev libreadline6-dev libreadline6 librrd-dev python-setuptools python-dev erlang-nox -y
+pkg-fix
 export DEBIAN_FRONTEND=noninteractive
 apt-get install mysql-server mysql-client mysql-common -y
 mysql-conn_test
+pkg-fix
 
 # Download Zenoss DEB and install it
 wget -N hydruid-blog.com/zenoss-core-4.2.4-1897.ubuntu.x86-64_01a_amd64.deb
@@ -48,9 +54,9 @@ give-props
 mysql -u root -e "create database zenoss_zep"
 mysql -u root -e "create database zodb"
 mysql -u root -e "create database zodb_session"
-mysql -u root zenoss_zep < /home/zenoss/zenoss_zep.sql
-mysql -u root zodb < /home/zenoss/zodb.sql
-mysql -u root zodb_session < /home/zenoss/zodb_session.sql
+mysql -u root zenoss_zep < $zenosshome/zenoss_zep.sql
+mysql -u root zodb < $zenosshome/zodb.sql
+mysql -u root zodb_session < $zenosshome/zodb_session.sql
 mysql -u root -e "CREATE USER 'zenoss'@'localhost' IDENTIFIED BY  'zenoss';"
 mysql -u root -e "GRANT REPLICATION SLAVE ON *.* TO 'zenoss'@'localhost' IDENTIFIED BY PASSWORD '*3715D7F2B0C1D26D72357829DF94B81731174B8C';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON zodb.* TO 'zenoss'@'localhost';"
@@ -65,8 +71,8 @@ mysql -u root -e "GRANT ALL PRIVILEGES ON zodb_session.* TO 'zenoss'@'%';"
 mysql -u root -e "GRANT SELECT ON mysql.proc TO 'zenoss'@'%';"
 
 # Rabbit install and config
-wget -N http://www.rabbitmq.com/releases/rabbitmq-server/v3.1.5/rabbitmq-server_3.1.5-1_all.deb -P /home/zenoss/zenoss424-srpm_install/
-dpkg -i /home/zenoss/zenoss424-srpm_install/rabbitmq-server_3.1.5-1_all.deb
+wget -N http://www.rabbitmq.com/releases/rabbitmq-server/v3.2.1/rabbitmq-server_3.2.1-1_all.deb -P $zenosshome/zenoss424-srpm_install/
+dpkg -i $zenosshome/zenoss424-srpm_install/rabbitmq-server_3.2.1-1_all.deb
 chown -R zenoss:zenoss $ZENHOME
 rabbitmqctl add_user zenoss zenoss
 rabbitmqctl add_vhost /zenoss
