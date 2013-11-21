b0db1a5a3c1f13f96de05b2c792c692af239392b
Trey H <treyh@ubuntu.(none)>
2013-11-21 09:23:44 -0600

    Version: 01c

--- core-autodeploy/4.2.4/build-deb/zenpack-helper.sh
+++ core-autodeploy/4.2.4/build-deb/zenpack-helper.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #######################################################
-# Version: 01b                                        #
+# Version: 01c                                        #
 #######################################################
 
-array=( PySamba*.egg WindowsMonitor*.egg ActiveDirectory*.egg IISMonitor*.egg MSExchange*.egg MSMQMonitor*.egg MSSQLServer*.egg ApacheMonitor*.egg DellMonitor*.egg DigMonitor*.egg DnsMonitor*.egg FtpMonitor*.egg HPMonitor*.egg HttpMonitor*.egg IRCDMonitor*.egg JabberMonitor*.egg LDAPMonitor*.egg MySqlMonitor*.egg NNTPMonitor*.egg NtpMonitor*.egg ZenJMX*.egg LinuxMonitor*.egg ZenossVirtualHostMonitor*.egg EsxTop*.egg XenMonitor*.egg DeviceSearch*.egg PythonCollector*.egg WBEM*.egg )
+array=( 'PySamba*.egg' 'WindowsMonitor*.egg' 'ActiveDirectory*.egg' 'IISMonitor*.egg' 'MSExchange*.egg' 'MSMQMonitor*.egg' 'MSSQLServer*.egg' 'ApacheMonitor*.egg' 'DellMonitor*.egg' 'DigMonitor*.egg' 'DnsMonitor*.egg' 'FtpMonitor*.egg' 'HPMonitor*.egg' 'HttpMonitor*.egg' 'IRCDMonitor*.egg' 'JabberMonitor*.egg' 'LDAPMonitor*.egg' 'MySqlMonitor*.egg' 'NNTPMonitor*.egg' 'NtpMonitor*.egg' 'ZenJMX*.egg' 'LinuxMonitor*.egg' 'ZenossVirtualHostMonitor*.egg' 'EsxTop*.egg' 'XenMonitor*.egg' 'DeviceSearch*.egg' 'PythonCollector*.egg' 'WBEM*.egg' )
 for i in "${array[@]}"
 do
 	zenpack --install /tmp/opt/zenoss/packs/ZenPacks.zenoss.$i
