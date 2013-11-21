9ee9f05c1c9bf83ab47965644c4228db4f0c50e8
Trey H <treyh@ubuntu.(none)>
2013-11-21 09:54:58 -0600

    Version: 01a

---
+++ core-autodeploy/4.2.4/build-deb/zenoss-4.2.4_install-zenpacks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#######################################################
+# Version: 01a                                        #
+#######################################################
+
+zenoss restart
+
+array=( PySamba*.egg WindowsMonitor*.egg ActiveDirectory*.egg IISMonitor*.egg MSExchange*.egg MSMQMonitor*.egg MSSQLServer*.egg ApacheMonitor*.egg DellMonitor*.egg DigMonitor*.egg DnsMonitor*.egg FtpMonitor*.egg HPMonitor*.egg HttpMonitor*.egg IRCDMonitor*.egg JabberMonitor*.egg LDAPMonitor*.egg MySqlMonitor*.egg NNTPMonitor*.egg NtpMonitor*.egg ZenJMX*.egg LinuxMonitor*.egg ZenossVirtualHostMonitor*.egg EsxTop*.egg XenMonitor*.egg DeviceSearch*.egg PythonCollector*.egg WBEM*.egg )
+for i in "${array[@]}"
+do
+	zenpack --install /tmp/opt/zenoss/packs/ZenPacks.zenoss.$i
+done
+
+easy_install readline
