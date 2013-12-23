f203f403946737ad7ab43e74c4953ee76bd955ea
Hydruid <trey.hunsucker@gmail.com>
2013-12-23 13:21:26 -0600

    01a Alpha01

---
+++ core-autodeploy/4.2.4/dev/variables.py
@@ -0,0 +1,10 @@
+#!/bin/bash
+#######################################################
+# Version: 01a Alpha01                                       
+#######################################################
+
+# Script Variables
+ZENHOME="/usr/local/zenoss"
+PYTHONPATH="/usr/local/zenoss/lib/python"
+PATH="/usr/local/zenoss/bin:$PATH"
+INSTANCE_HOME="$ZENHOME"
