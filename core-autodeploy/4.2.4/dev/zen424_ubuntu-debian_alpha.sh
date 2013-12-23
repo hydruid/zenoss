fc383b58b5a5def8568fb722150592f6433951fd
Hydruid <trey.hunsucker@gmail.com>
2013-12-23 14:11:51 -0600

    01a Alpha03

--- core-autodeploy/4.2.4/dev/zen424_ubuntu-debian_alpha.sh
+++ core-autodeploy/4.2.4/dev/zen424_ubuntu-debian_alpha.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env python
 #######################################################
-# Version: 01a Alpha02                                #
-#  Status: Not Functional                             #
-#   Notes: Converting to Python                       #
-#  Zenoss: Core 4.2.4 & ZenPacks (v1897)              #
-#      OS: Ubuntu/Debian x86_64 (requires 64-bit os)  #
+# Version: 01a Alpha03
+#  Status: Not Functional                             
+#   Notes: Converting to Python                       
+#  Zenoss: Core 4.2.4 & ZenPacks (v1897)              
+#      OS: Ubuntu/Debian x86_64 (requires 64-bit os) 
 #######################################################
 
 import sys
 import os
 import time
 import urllib
+import pwd
+import grp
 
 # Script variables
 zenosshome="/home/zenoss"
 downdir="/tmp"
-varurl="https://raw.github.com/hydruid/zenoss/master/core-autodeploy/4.2.4/misc/variables.sh"
+varurl="https://raw.github.com/hydruid/zenoss/master/core-autodeploy/4.2.4/dev/variables.py"
+zenuid=pwd.getpwnam("zenoss").pw_uid
+zengid=grp.getgrnam("zenoss").gr_gid
 
 # Message
 os.system('clear')
@@ -33,12 +37,24 @@ with open(os.path.join(zenosshome, '.bashrc'), "a") as f:
     f.write("export ZENHOME=/usr/local/zenoss\nexport PYTHONPATH=/usr/local/zenoss/lib/python\nexport PATH=/usr/local/zenoss/bin:$PATH\nexport INSTANCE_HOME=$ZENHOME\nexport PATH=/opt/zenup/bin:$PATH")
 if not os.path.exists(os.path.join(zenosshome, 'zenoss424-srpm_install')):
     os.makedirs(os.path.join(zenosshome, 'zenoss424-srpm_install'))
-urllib.urlretrieve(varurl, os.path.join(zenosshome, "zenoss424-srpm_install/variables.sh"))
+urllib.urlretrieve(varurl, os.path.join(zenosshome, "zenoss424-srpm_install/variables.py"))
+execfile("/home/zenoss/zenoss424-srpm_install/variables.py")
+if not os.path.exists(ZENHOME):
+    os.makedirs(ZENHOME)
+os.chown(ZENHOME, zenuid, zengid)
 
+# OS Compatibility Tests
+if detectos('/etc/issue.net', 'Ubuntu 13'):
+    print "...Supported OS detected"
+elif detectos('/etc/issue.net', 'Ubuntu 12'):
+    print "...Supported OS detected"
+elif detectos('/etc/issue.net', 'Debian 7'):
+    print "...Supported OS detected"
+else:
+    sys.exit("...OS is not supported")
 
+#detect-arch && detect-user
 
-#. $zenosshome/zenoss424-srpm_install/variables.sh
-#mkdir $ZENHOME && chown -cR zenoss:zenoss $ZENHOME
 
 
 exit()
