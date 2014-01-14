02d3f1344a2ef36438e41bc1e17d9ed5c9da2cef
Hydruid <trey.hunsucker@gmail.com>
2014-01-14 13:36:46 -0600

    01a Alpha04

--- core-autodeploy/4.2.4/dev/variables.py
+++ core-autodeploy/4.2.4/dev/variables.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #######################################################
-# Version: 01a Alpha03                                       
+# Version: 01a Alpha04                                       
 #######################################################
 
 # Script Variables
@@ -12,7 +12,8 @@ INSTANCE_HOME="$ZENHOME"
 ## Misc
 ARCH=struct.calcsize("P") * 8
 USER=os.getlogin()
-UBUNTUPKGS=os.system('apt-get install rrdtool libmysqlclient-dev nagios-plugins erlang subversion autoconf swig unzip zip g++ libssl-dev maven libmaven-compiler-plugin-java build-essential libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev oracle-java7-installer python-twisted python-gnutls python-twisted-web python-samba libsnmp-base snmp-mibs-downloader bc rpm2cpio memcached libncurses5 libncurses5-dev libreadline6-dev libreadline6 librrd-dev python-setuptools python-dev erlang-nox -y')
+UBUNTUPKGS=os.system('apt-get install rrdtool libmysqlclient-dev nagios-plugins erlang subversion autoconf swig unzip zip g++ libssl-dev maven libmaven-compiler-plugin-java build-essential libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev oracle-java7-installer python-twisted python-gnutls python-twisted-web python-samba libsnmp-base snmp-mibs-downloader bc rpm2cpio memcached libncurses5 libncurses5-dev libreadline6-dev libreadline6 librrd-dev python-setuptools python-dev erlang-nox python-mysqldb -y')
+DEBIANPKGS=os.system('apt-get install rrdtool libmysqlclient-dev nagios-plugins erlang subversion autoconf swig unzip zip g++ libssl-dev maven libmaven-compiler-plugin-java build-essential libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev oracle-java7-installer python-twisted python-gnutls python-twisted-web python-samba libsnmp-base bc rpm2cpio memcached libncurses5 libncurses5-dev libreadline6-dev libreadline6 librrd-dev python-setuptools python-dev erlang-nox smistrip -y')
 PKGFIX=os.system('apt-get -f install')
 
 # Functions
