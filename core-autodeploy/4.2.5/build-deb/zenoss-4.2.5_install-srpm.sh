721b2dba8312a0cdf1a8491c7c7e6629671412af
Trey <treyh@ubuntu.(none)>
2014-01-30 12:42:53 -0600

    01a Alpha01

--- core-autodeploy/4.2.5/build-deb/zenoss-4.2.5_install-srpm.sh
+++ core-autodeploy/4.2.5/build-deb/zenoss-4.2.5_install-srpm.sh
@@ -37,7 +37,7 @@ mkdir $ZENHOME && chown -cR zenoss:zenoss $ZENHOME
 
 # OS compatibility tests
 detect-os2 && detect-arch && detect-user
-if grep -Fxq "Ubuntu 12.04.3 LTS" /etc/issue.net
+if grep -Fxq "Ubuntu 12.04.4 LTS" /etc/issue.net
         then    echo "...Correct OS detected."
 else    echo "...Incorrect OS detected, this build script requires Ubuntu 12.04 LTS" && exit 0
 fi
