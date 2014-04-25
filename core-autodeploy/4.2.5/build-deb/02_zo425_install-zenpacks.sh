4d64241320c92c42fc4d7d48fd233afc71a1a83b
Hydruid <trey.hunsucker@gmail.com>
2014-04-25 10:41:37 -0500

    02b

--- core-autodeploy/4.2.5/build-deb/02_zo425_install-zenpacks.sh
+++ core-autodeploy/4.2.5/build-deb/02_zo425_install-zenpacks.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ##########################################
-# Version: 01a
+# Version: 01b
 #  Status: Functional
 #   Notes: Testing out 4.2.5
 ##########################################
 
 read -p "This script needs to run as the zenoss user and zenoss must be running. Press ctrl+c to cancel if needed..."
-array=( PySamba*.egg WindowsMonitor*.egg ActiveDirectory*.egg IISMonitor*.egg MSExchange*.egg MSMQMonitor*.egg MSSQLServer*.egg ApacheMonitor*.egg DellMonitor*.egg DigMonitor*.egg DnsMonitor*.egg FtpMonitor*.egg HPMonitor*.egg HttpMonitor*.egg IRCDMonitor*.egg JabberMonitor*.egg LDAPMonitor*.egg MySqlMonitor*.egg NNTPMonitor*.egg NtpMonitor*.egg ZenJMX*.egg LinuxMonitor*.egg ZenossVirtualHostMonitor*.egg EsxTop*.egg XenMonitor*.egg DeviceSearch*.egg PythonCollector*.egg WBEM*.egg Microsoft.Windows*.egg )
+array=( PySamba*.egg WindowsMonitor*.egg ActiveDirectory*.egg IISMonitor*.egg MSExchange*.egg MSMQMonitor*.egg MSSQLServer*.egg ApacheMonitor*.egg DellMonitor*.egg DigMonitor*.egg DnsMonitor*.egg FtpMonitor*.egg HPMonitor*.egg HttpMonitor*.egg IRCDMonitor*.egg JabberMonitor*.egg LDAPMonitor*.egg NNTPMonitor*.egg NtpMonitor*.egg ZenJMX*.egg LinuxMonitor*.egg ZenossVirtualHostMonitor*.egg EsxTop*.egg XenMonitor*.egg DeviceSearch*.egg PythonCollector*.egg MySqlMonitor*.egg WBEM*.egg Microsoft.Windows*.egg )
 for i in "${array[@]}"
 do
 	zenpack --install /tmp/opt/zenoss/packs/ZenPacks.zenoss.$i
