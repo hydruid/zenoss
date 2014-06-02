f6e9b03534bc9ed3c5d343fa539b160f9fa7bf8b
Hydruid <trey.hunsucker@gmail.com>
2014-06-02 11:12:18 -0500

    01a

--- zenpacks/DiscoveryH/switch.sh
+++ zenpacks/DiscoveryH/switch.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 ##########################################
-# Version: 01a Alpha02
-#  Status: Functional but not for Production
-#   Notes: Under Construction
+# Version: 01a
+#  Status: Functional
+#   Notes: Still under development 
 ##########################################
 
 # Variables
-## SNMP
+##SNMP
 SNMPVER="2c"
+
+# Query Order
+##Make, Model, SN, Location, IP Address, Version
 
 # Cisco Query
 if [ $3 = "c" ]; then
@@ -24,8 +27,21 @@ if [ $3 = "d" ]; then
         echo "Dell"
         snmpwalk -v$SNMPVER -c$1 $2 1.3.6.1.2.1.47.1.1.1.1.11 | grep -m 1 STRING | grep -o "[^ ]*$" | tr -d '"'
         snmpwalk -v$SNMPVER -c$1 $2 1.3.6.1.2.1.47.1.1.1.1.13.2 | grep -m 1 STRING | grep -o "[^ ]*$" | tr -d '"'
+        snmpwalk -v$SNMPVER -c$1 $2 1.3.6.1.2.1.1.6.0 | grep -m 1 STRING | grep -o '"[^"]\+"' |  tr -d '"'
+        snmpwalk -v$SNMPVER -c$1 $2 1.3.6.1.2.1.4.20.1.1 | awk 'NF>1{print $NF}'
+        snmpwalk -v$SNMPVER -c$1 $2 1.3.6.1.2.1.1.1.0 | grep -m 1 STRING | awk '{print $6}' | tr -d ','
 fi
 
 
 exit 0
+
+# SNMP Config Examples
+##Cisco
+#snmp-server community pencil RO
+#snmp-server location OKC Room#_IDF-#_RU#
+
+##Dell
+#snmp-server community pencil ro
+#snmp-server location "OKC Room#_IDF-#_RU#"
+
 
